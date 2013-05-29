8a7f5760997a12ea15d8e85663163fd56abfb9cf
Erik de Jonge <erik@a8.nl>
2013-05-29 11:41:35 +0200

    login font

--- source/commands/file_copy_test.py
+++ source/commands/file_copy_test.py
@@ -1,10 +1,18 @@
 # coding=utf-8
 import os
-
+import stat
 def main():
     stats = os.stat("./gzip")
     open("gzip_copy", "w").write(open("./gzip", "r").read())
     stats2 = os.stat("./gzip_copy")
+    print stats
+    print stats2
+
+
+    print
+    mode = stats.st_mode
+    print mode
+    os.chmod("./gzip_copy", mode)
     print stats
     print stats2
 
