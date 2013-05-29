7c189f4e474eb8cb9e11f522d7eb51d2eb9778d2
Erik de Jonge <erik@a8.nl>
2013-05-29 12:42:56 +0200

    x

--- source/commands/file_copy_test.py
+++ source/commands/file_copy_test.py
@@ -1,20 +1,35 @@
 # coding=utf-8
 import os
 import stat
+
 def main():
     stats = os.stat("./gzip")
-    open("gzip_copy", "w").write(open("./gzip", "r").read())
-    stats2 = os.stat("./gzip_copy")
-    print stats
-    print stats2
+    open("gzip_copy2", "w").write(open("./gzip", "r").read())
+    stats2 = os.stat("./gzip_copy2")
+    #print stats
+    #print stats2
 
-
-    print
     mode = stats.st_mode
-    print mode
-    os.chmod("./gzip_copy", mode)
+    os.chmod("./gzip_copy2", mode)
+    uid = stats.st_uid
+    gid = stats.st_gid
+    #os.chown("./gzip_copy2", uid, gid)
+        
+
+    atime = stats[stat.ST_ATIME]
+    mtime = stats[stat.ST_MTIME]
+    os.utime("./gzip_copy2", (atime, mtime))
+
+
+
+
+    print "----------"
+    stats = os.stat("./gzip")
+    stats2 = os.stat("./gzip_copy2")
     print stats
     print stats2
 
-if __name__=="__main__":
+    os.system("ls -las gzip*")
+
+if __name__ == "__main__":
     main()
