fff3d8fca661e7898324badedcfa72bfa7786dd5
Erik de Jonge <erik@a8.nl>
2013-07-31 23:28:49 -1000

    -

--- buildcommands.sh
+++ buildcommands.sh
@@ -1,15 +1,15 @@
 
 rm ./build/$1/*.pyz
-rm ./dist/$1 
+rm ./dist/$1
 rm ./Cryptobox/Resources/$1
 echo "building", ./source/commands/$1.py
-python2.7.apple ./pyinstaller-2.0/pyinstaller.py --onefile --console --noupx ./source/commands/$1.py
+python ./pyinstaller-2.0/pyinstaller.py --onefile --console --noupx ./source/commands/$1.py
 mv $1.spec ./source/commands/specs
 mkdir -p ./Cryptobox/commands
-cp ./dist/$1 ./Cryptobox/commands    
+cp ./dist/$1 ./Cryptobox/commands
 echo ./dist/$1
 
 
- 
+
 
 
