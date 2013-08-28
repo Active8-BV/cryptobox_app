3b7d357e242be5dd7e833b2c2c146cc911860563
Erik de Jonge <erik@a8.nl>
2013-08-28 12:39:02 +0200

    -

---
+++ source/commands/tests.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+"""
+unit test for app commands
+"""
+__author__ = 'rabshakeh'
+import unittest
+from __init__ import *
+
+class CryptoboxAppTest(unittest.TestCase):
+    """
+    CryptoboTestCase
+    """
+
+    def setUp(self):
+        """
+        setUp
+        """
+
+    def tearDown(self):
+        """
+        tearDown
+        """
+
+    def test_index(self):
+        """
+        test_task
+        """
+
+
+if __name__ == '__main__':
+    unittest.main()
