ebddc8773c112cdfafc9dc9ad755a33fb1130809
Erik de Jonge <erik@a8.nl>
2013-08-30 15:40:26 +0200

    -

--- source/commands/tests.py
+++ source/commands/tests.py
@@ -15,7 +15,7 @@ from cba_index import make_local_index, index_and_encrypt
 from cba_memory import Memory
 from cba_blobs import get_blob_dir, get_data_dir
 from cba_network import authorize_user, authorized
-from cba_sync import get_server_index
+from cba_sync import get_server_index, parse_serverindex, parse_made_local, parse_removed_local
 from cba_file import ensure_directory
 
 
@@ -67,7 +67,6 @@ class CryptoboxAppTestBasic(unittest.TestCase):
         """
         if self.memory.has("localindex"):
             self.memory.delete("localindex")
-
         self.memory.save(get_data_dir(self.cboptions))
 
         os.system("rm -Rf testdata/testmap")
@@ -96,7 +95,6 @@ class CryptoboxAppTestBasic(unittest.TestCase):
         """
         test_index_and_encrypt
         """
-
         localindex = make_local_index(self.cboptions)
         self.memory.replace("localindex", localindex)
         index_and_encrypt(self.cboptions)
@@ -104,11 +102,14 @@ class CryptoboxAppTestBasic(unittest.TestCase):
 
         # add a new file
         open(os.path.join(self.cboptions.dir, "hello world.txt"), "w").write("hello world 123 Dit is random data")
+
         index_and_encrypt(self.cboptions)
         self.assertEqual(count_files_dir(get_blob_dir(self.cboptions)), 21)
 
         # same content, blob count should not raise
+
         open(os.path.join(self.cboptions.dir, "hello world2.txt"), "w").write("hello world 123 Dit is random data")
+
         index_and_encrypt(self.cboptions)
         self.assertEqual(count_files_dir(get_blob_dir(self.cboptions)), 21)
 
@@ -140,10 +141,12 @@ class CryptoboxAppTestServer(unittest.TestCase):
 
         self.cboptions = dict2obj_new(self.options_d)
         self.memory = Memory()
+        self.memory.set("cryptobox_folder", self.cboptions.dir)
         ensure_directory(self.cboptions.dir)
         ensure_directory(get_data_dir(self.cboptions))
         self.pipe = Popen("python server/manage.py runserver 127.0.0.1:8000", shell=True, cwd="/Users/rabshakeh/workspace/cryptobox/www_cryptobox_nl")
         django_starting = True
+
         while django_starting:
             try:
                 requests.get("http://127.0.0.1:8000")
@@ -156,6 +159,7 @@ class CryptoboxAppTestServer(unittest.TestCase):
         tearDown
         """
         djangopid = os.popen("ps aux | grep manage").read()
+
         for l in djangopid.split("\n"):
             if "runserver 127.0.0.1:8000" in str(l):
                 for i in range(0, 10):
@@ -165,7 +169,8 @@ class CryptoboxAppTestServer(unittest.TestCase):
         if self.memory.has("localindex"):
             self.memory.delete("localindex")
         self.memory.save(get_data_dir(self.cboptions))
-        os.system("rm -Rf testdata/testmap")
+
+        #os.system("rm -Rf testdata/testmap")
 
     def test_new_tree(self):
         self.pipe = Popen("python server/manage.py load -c test", shell=True, cwd="/Users/rabshakeh/workspace/cryptobox/www_cryptobox_nl")
@@ -186,11 +191,32 @@ class CryptoboxAppTestServer(unittest.TestCase):
         """
         test_get_server_tree
         """
+        os.system("cd testdata; unzip -o testmap.zip > /dev/null")
         self.memory = authorize_user(self.memory, self.cboptions)
         self.assertTrue(self.memory.get("authorized"))
         self.memory = get_server_index(self.memory, self.cboptions)
-        pass
+        serverindex = self.memory.get("serverindex")
+        serverindex_test = pickle.load(open("testdata/serverindex_test.pickle"))
+        self.maxDiff = None
+        self.assertEqual(serverindex, serverindex_test)
 
+    def test_compare_server_with_local_tree(self):
+        """
+        test_compare_server_with_local_tree
+        """
+        self.memory = get_server_index(self.memory, self.cboptions)
+        serverindex = self.memory.get("serverindex")
+        dirname_hashes_server, fnodes, unique_content, unique_dirs = parse_serverindex(serverindex)
+        self.assertEqual(len(dirname_hashes_server), 4)
+        self.assertEqual(len(fnodes), 9)
+        self.assertEqual(len(unique_content), 4)
+
+        self.memory.set("localindex", make_local_index(self.cboptions))
+        dirs_to_make_on_server, dirs_to_remove_locally = parse_made_local(self.memory, self.cboptions, dirname_hashes_server, serverindex)
+        self.assertEqual(len(dirs_to_make_on_server), 0)
+        self.assertEqual(len(dirs_to_remove_locally), 0)
+        dir_names_to_delete_on_server, memory = parse_removed_local(self.memory, self.cboptions, unique_dirs)
+        self.assertEqual(len(dir_names_to_delete_on_server), 0)
 
 
 if __name__ == '__main__':
