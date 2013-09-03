c3ded9f5d9dc9fc6fda3399bfb9ad7df12aee3cb
Erik de Jonge <erik@a8.nl>
2013-09-03 18:09:16 +0200

    -

--- source/commands/cba_sync.py
+++ source/commands/cba_sync.py
@@ -51,7 +51,7 @@ def get_unique_content(memory, options, all_unique_nodes, local_file_paths):
     @type local_file_paths: list
     """
     if len(local_file_paths) == 0:
-        return
+        return memory
 
     unique_nodes_hashes = [fhash for fhash in all_unique_nodes if not have_blob(options, fhash)]
     unique_nodes = [all_unique_nodes[fhash] for fhash in all_unique_nodes if fhash in unique_nodes_hashes]
