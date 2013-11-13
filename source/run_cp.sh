458937e324ec3e6ab1258c1982fa1b98703f8a1c
Erik de Jonge <erik@a8.nl>
2013-11-13 09:24:40 +0100

    cryptobox.js

--- source/run_cp.sh
+++
@@ -1,6 +0,0 @@
-python -OO ./commands/cp.py -r 0 -f ./cryptobox.coffee
-coffee -c -b ./cryptobox.coffee;
-
-
-
-
--- source/run_node_test.sh
+++ source/run_node_test.sh
@@ -1,2 +1,3 @@
+python -OO ./commands/cp.py -r 0 -f ./node_test.coffee
 coffee -c -b ./node_test.coffee
 node node_test.js
