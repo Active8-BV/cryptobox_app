024ef45073845f28662c0ac3dd3bcc80b3d7d8db
Erik de Jonge <erik@a8.nl>
2013-11-29 10:08:02 +0100

    Fri Nov 29 10:08:02 CET 2013

--- quotes/download_searchquotes.py
+++ quotes/download_searchquotes.py
@@ -1,18 +1,19 @@
 # coding=utf-8
-import urllib
 import BeautifulSoup
+
 l = []
 for i in range(1, 21):
     break
     #u = "http://www.searchquotes.com/search/Privacy/"+str(i)+"/"
     #c = urllib.urlopen(u).read()
 
-    c = open("./searchquotes/"+str(i)+".html").read()
+    c = open("./searchquotes/" + str(i) + ".html").read()
     b = BeautifulSoup.BeautifulSoup(c)
     item = {}
 
     for a in b.findAll("a"):
 
+        #noinspection PyBroadException
         try:
             if a["class"] == "mainquote":
                 item["text"] = a["title"]
@@ -21,10 +22,9 @@ for i in range(1, 21):
 
                 l.append(item)
                 item = {}
-
-
         except:
             pass
 import pickle
+
 open("list.pickle", "w").write(pickle.dumps(l))
 
