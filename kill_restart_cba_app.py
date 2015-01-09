8df88356784891c1a9843180a79acc77f04b8930
Erik de Jonge <erik@a8.nl>
2015-01-09 10:34:36 +0100

    Fri Jan  9 10:34:36 CET 2015

--- kill_restart_cba_app.py
+++ kill_restart_cba_app.py
@@ -1,27 +1,33 @@
+
 import os
 import time
 from multiprocessing import Pool
 
+
 def bring_to_front():
+    """
+    bring_to_front
+    """
     time.sleep(0.3)
     os.system("osascript node_webkit_to_foreground.scpt")
 
+
 if __name__ == '__main__':
     pool = Pool(processes=1)              # Start a worker processes.
-
 
     if os.path.exists("app.running"):
         exit(1)
     try:
         while True:
             if not os.path.exists("app.running"):
-                os.system("killall node-webkit")                
+                os.system("killall node-webkit")
                 open("app.running", "w").write(".")
-                pool.apply_async(bring_to_front)                
+                pool.apply_async(bring_to_front)
                 os.system("make run")
+
                 if os.path.exists("app.running"):
                     os.remove("app.running")
+
             print "restarting app"
-            
     finally:
         os.remove("app.running")
